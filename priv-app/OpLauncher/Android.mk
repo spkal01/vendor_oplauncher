b5be7272e798e4b02095b658bda8ef70dac2409f
spkal01 <kalligeross@gmail.com>
2021-01-29 08:22:10 +0100

    Fix some deprs

--- priv-app/OpLauncher/Android.mk
+++ priv-app/OpLauncher/Android.mk
@@ -1,12 +1,12 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := OPLauncher
+LOCAL_MODULE := OpLauncher
 LOCAL_MODULE_STEM := OpLauncher.apk
 LOCAL_SRC_FILES := OpLauncher.apk
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_OUT)/priv-app/OPLauncher
+LOCAL_MODULE_PATH := $(TARGET_OUT)/priv-app/OpLauncher
 LOCAL_OVERRIDES_PACKAGES := ParanoidQuickStep Launcher3QuickStep Launcher3 PixelLauncher NexusLauncherRelease TrebuchetQuickStep
 
 include $(BUILD_PREBUILT)
